d301d1735e7fdcbc9a4688de08f0c982181d54ee
Daniel Kognita <77123407+danielkognita@users.noreply.github.com>
2021-07-19 12:01:28 -0300

    docs(dump_banco_dados): dump data base

---
+++ backend/dump_banco/temrazao.sql
@@ -0,0 +1,219 @@
+CREATE DATABASE  IF NOT EXISTS `temrazao` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `temrazao`;
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: temrazao
+-- ------------------------------------------------------
+-- Server version	8.0.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `client_addresses`
+--
+
+DROP TABLE IF EXISTS `client_addresses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `client_addresses` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `street` varchar(255) NOT NULL,
+  `number` int NOT NULL,
+  `complement` varchar(50) NOT NULL,
+  `neighborhood` varchar(255) NOT NULL,
+  `zipcode` varchar(8) NOT NULL COMMENT 'the zipcode must have only numbers',
+  `city` varchar(255) NOT NULL,
+  `state` varchar(2) NOT NULL,
+  `is_default` tinyint NOT NULL,
+  `is_active` tinyint NOT NULL DEFAULT '1',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `client_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_a88557f9d4f0eebd10e1c46af73` (`client_id`),
+  CONSTRAINT `FK_a88557f9d4f0eebd10e1c46af73` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `client_addresses`
+--
+
+LOCK TABLES `client_addresses` WRITE;
+/*!40000 ALTER TABLE `client_addresses` DISABLE KEYS */;
+/*!40000 ALTER TABLE `client_addresses` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `clients`
+--
+
+DROP TABLE IF EXISTS `clients`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `clients` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `company_name` varchar(255) NOT NULL,
+  `person_in_charge` varchar(255) NOT NULL,
+  `person_in_charge_document` varchar(11) NOT NULL COMMENT 'the client person in charge document or (cpf) must contains only numbers',
+  `is_active` tinyint NOT NULL DEFAULT '1',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clients`
+--
+
+LOCK TABLES `clients` WRITE;
+/*!40000 ALTER TABLE `clients` DISABLE KEYS */;
+INSERT INTO `clients` VALUES (1,'Daniel','Silva','13734558262',1,'2021-06-28 18:19:28','2021-06-28 18:19:28');
+/*!40000 ALTER TABLE `clients` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `modules`
+--
+
+DROP TABLE IF EXISTS `modules`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `modules` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `IDX_8cd1abde4b70e59644c98668c0` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `modules`
+--
+
+LOCK TABLES `modules` WRITE;
+/*!40000 ALTER TABLE `modules` DISABLE KEYS */;
+INSERT INTO `modules` VALUES (1,'Client','[NULL]'),(2,'User','[NULL]'),(6,'all','[NULL]'),(8,'ClientAddress','[NULL]');
+/*!40000 ALTER TABLE `modules` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `permissions`
+--
+
+DROP TABLE IF EXISTS `permissions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `permissions` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `action` varchar(255) NOT NULL,
+  `conditions` json DEFAULT NULL,
+  `subject` varchar(255) DEFAULT NULL,
+  `role_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `IDX_af6ab46bfc325cb7177ddb5752` (`action`,`subject`,`role_id`),
+  KEY `FK_aa938076aecb265e4446874d3a2` (`subject`),
+  KEY `FK_f10931e7bb05a3b434642ed2797` (`role_id`),
+  CONSTRAINT `FK_aa938076aecb265e4446874d3a2` FOREIGN KEY (`subject`) REFERENCES `modules` (`name`),
+  CONSTRAINT `FK_f10931e7bb05a3b434642ed2797` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `permissions`
+--
+
+LOCK TABLES `permissions` WRITE;
+/*!40000 ALTER TABLE `permissions` DISABLE KEYS */;
+INSERT INTO `permissions` VALUES (1,'manage','{}','all',1),(2,'read','{\"clientEager.id\": \"{user.clientEager.id}\"}','User',2),(3,'update','{\"clientEager.id\": \"{user.clientEager.id}\"}','User',2),(4,'create','{\"clientEager.id\": \"{user.clientEager.id}\"}','User',2),(5,'delete','{\"clientEager.id\": \"{user.clientEager.id}\"}','User',2),(14,'read','{\"id\": \"{user.clientEager.id}\"}','Client',2),(15,'update','{\"id\": \"{user.clientEager.id}\"}','Client',2),(17,'read','{\"client.id\": \"{user.clientEager.id}\"}','ClientAddress',2),(18,'create','{\"client.id\": \"{user.clientEager.id}\"}','ClientAddress',2),(19,'update','{\"client.id\": \"{user.clientEager.id}\"}','ClientAddress',2),(20,'delete','{\"client.id\": \"{user.clientEager.id}\"}','ClientAddress',2);
+/*!40000 ALTER TABLE `permissions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `roles` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `IDX_648e3f5447f725579d7d4ffdfb` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES (1,'admin'),(2,'aluno');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `role_id` int DEFAULT NULL,
+  `client_id` int DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `last_login` timestamp NULL DEFAULT NULL,
+  `avatar` varchar(255) DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `remember_token` varchar(255) DEFAULT NULL,
+  `settings` varchar(255) DEFAULT NULL,
+  `is_active` tinyint NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `IDX_97672ac88f789774dd47f7c8be` (`email`),
+  KEY `FK_a2cecd1a3531c0b041e29ba46e1` (`role_id`),
+  KEY `FK_0d1e90d75674c54f8660c4ed446` (`client_id`),
+  CONSTRAINT `FK_0d1e90d75674c54f8660c4ed446` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`),
+  CONSTRAINT `FK_a2cecd1a3531c0b041e29ba46e1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (3,'Admin','User',1,1,'danielsilva.2013@alunos.utfpr.edu.br','2021-06-28 18:16:59','2021-06-28 18:16:59','2021-07-19 14:52:53',NULL,'$2b$08$lhT3ohnyTuM6O2kRQFogLuIFfp4eZNJyz.mu1EHO7imKJkNo92vF.',NULL,NULL,NULL,1);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-07-19 11:56:43
